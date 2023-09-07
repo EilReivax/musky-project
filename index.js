566493213817be1d049bc0accd83b938e652274a
mlie0008 <mlie0008@student.monash.edu>
2023-09-07 19:07:48 +1000

    Added example

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const PORT_NUMBER = 8080;
 
 let app = express();
+let users = [];
 
 app.use(express.static("node_modules/bootstrap/dist/css"));
 app.use(express.static("images"));
@@ -20,4 +21,30 @@ app.listen(PORT_NUMBER, function () {
 // Home Page
 app.get("/", function (request, response) {
 	response.render("index");
+});
+
+app.get('/view-all', function (req, res) {
+    let st = 'ID    Name</br>';
+    for (let i = 0; i < db.length; i++) {
+        st += db[i].id + ' | ' + db[i].userName + '</br>';
+    }
+    return st;
+});
+
+app.get("/add-user/:userName/", function (req, res) {
+    let id = Math.round(Math.random()*1000);
+    let userName = req.params.userName;
+    let newUser = {id: id, userName: userName};
+    users.push(newUser);
+    res.redirect("/view-all");
+});
+
+app.get('/delete-user', function (req, res) {
+    let id = req.query.id;
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].id == id) {
+            users.splice(i, 1);
+        }
+    }
+    res.redirect("/view-all");
 });
