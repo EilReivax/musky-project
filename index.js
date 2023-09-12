59ea8012a4a2c12b660f3715d27096eff20c70f6
Michael  Xavier Lie <mlie0008@student.monash.edu>
2023-09-12 20:10:46 +1000

    Added edit task and edit category

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const PORT_NUMBER = 8080;
 let app = express();
 let categories = [];
 let tasks = [];
+let categoryIdCounter = 1;
+let taskIdCounter = 1;
 
 app.use(express.static("node_modules/bootstrap/dist/css"));
 app.use(express.static("images"));
@@ -20,15 +22,15 @@ app.listen(PORT_NUMBER, function () {
 	console.log(`listening on port ${PORT_NUMBER}`);
 });
 
+categories.push(new Category(generateCategoryId(), "To Do"));
+categories.push(new Category(generateCategoryId(), "Doing"));
+categories.push(new Category(generateCategoryId(), "Done"));
+
 // Home Page
 app.get("/", function (req, res) {
-    if (categories.length == 0) {
-        categories.push(new Category(generateCategoryId(), "To Do"));
-        categories.push(new Category(generateCategoryId(), "Doing"));
-        categories.push(new Category(generateCategoryId(), "Done"));
-    }
     console.log(categories)
     console.log(tasks)
+
 	res.render("index", { categories: categories, tasks: tasks });
 });
 
@@ -40,19 +42,11 @@ app.get("/add-category", function (req, res) {
 app.post("/add-category", function (req, res) {
     let id = generateCategoryId();
     let title = req.body.title;
+
     categories.push(new Category(id, title));
 
     res.redirect("/");
 });
-
-// Edit Category Page
-app.get("/edit-category/:id", function (req, res) {
-    let id = req.params.id;
-    let category = categories.find(category => category.id == id);
-});
-
-// Delete Category
-
 
 // Add Task Page
 app.get('/add-task', function (req, res) {
@@ -63,11 +57,30 @@ app.post("/add-task", function (req, res) {
     let id = generateTaskId();
     let title = req.body.title;
     let description = req.body.description;
-    let dueDate = req.body.dueDate;
-    let priority = req.body.priority;
+    let dueDate = new Date(req.body.dueDate);
+    let priority = parseInt(req.body.priority);
     let categoryId = req.body.categoryId;
     
     tasks.push(new Task(id, title, description, dueDate, priority, categoryId));
+
+    res.redirect("/");
+});
+
+// Edit Category Page
+app.get("/edit-category/:id", function (req, res) {
+    let id = req.params.id;
+    let category = categories.find(category => category.id == id);
+    console.log(category)
+
+    res.render("edit-category", { category: category });
+});
+
+app.post("/edit-category/:id", function (req, res) {
+    let id = req.params.id;
+    let category = categories.find(category => category.id == id);
+
+    category.title = req.body.title;
+    
     res.redirect("/");
 });
 
@@ -75,28 +88,61 @@ app.post("/add-task", function (req, res) {
 app.get("edit-task/:id", function (req, res) {
     let id = req.params.id;
     let task = tasks.find(task => task.id == id);
+
     res.render("edit-task", {task: task, categories: categories});
 });
 
-app.post("/edit-task/", function (req, res) {
+app.post("/edit-task/:id", function (req, res) {
+    let id = req.params.id;
+    let task = tasks.find(task => task.id == id);
 
+    task.title = req.body.title;
+    task.description = req.body.description;
+    task.dueDate = new Date(req.body.dueDate);
+    task.priority = parseInt(req.body.priority);
+    task.categoryId = req.body.categoryId;
+
+    res.redirect("/");
+});
+
+// Delete Category
+app.post("/delete-category/:id", function (req, res) {
+    let id = req.params.id;
+    let category = categories.find(category => category.id == id)
+    let categoryIndex = categories.indexOf(category);
+
+    categories.splice(categoryIndex, 1);
+
+    for (let i = 0; i < tasks.length; i++) {
+        if (task[i].categoryId == category.id) {
+            tasks.splice(i, 1);
+        }
+    }
+
+    res.redirect("/");
 });
 
 // Delete Task
+app.post("/delete-task/:id", function (req, res) {
+    let id = req.params.id;
+    let taskIndex = tasks.indexOf(tasks.find(task => task.id == id))
 
+    tasks.splice(taskIndex, 1);
+
+    res.redirect("/");
+});
 
 // Functions
-let categoryIdCounter = 1;
-let taskIdCounter = 1;
-
 function generateCategoryId() {
     const categoryId = `C-${categoryIdCounter}`;
     categoryIdCounter++;
+
     return categoryId;
 }
 
 function generateTaskId() {
     const taskId = `T-${taskIdCounter}`;
     taskIdCounter++;
+
     return taskId;
 }
