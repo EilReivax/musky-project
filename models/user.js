f793f2ba2088b30e6476af407d800ec82b40ad03
Michael  Xavier Lie <mlie0008@student.monash.edu>
2023-10-02 23:41:24 +1100

    Added user class

---
+++ models/user.js
@@ -0,0 +1,9 @@
+class User {
+    constructor(username, password, firstName, lastName, phoneNumber) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+}
