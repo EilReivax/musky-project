4d5b2f7c55fdef3218589fe888c57f70d8eca309
Michael  Xavier Lie <mlie0008@student.monash.edu>
2023-10-17 01:55:30 +1100

    Added try catch

--- routers/category.js
+++ routers/category.js
@@ -1,34 +1,63 @@
-const Category = require("../models/category");
-const User = require("../models/user");
+const Category = require('../models/category');
+const User = require('../models/user');
 
 module.exports = {
     createOne: async function (req, res) {
-        let category = new Category(req.body);
-        await category.save();
+        try {
+            let category = new Category(req.body);
+            await category.save();
+        } catch (error) {
+            console.log(error);
+        }
+
         res.redirect('/dashboard');
     },
 
     getAll: async function (req, res) {
-        let categories = await Category.find()
-            .populate('taskList')
+        let categories = []
+        let users = []
+
+        try {
+            categories = await Category.find()
+            .populate({
+                path: 'taskList',
+                model: 'Task',
+                populate: {
+                    path: 'userList',
+                    model: 'User'
+                }
+            })
             .exec();
         
-        let users = await User.find().exec();
+            users = await User.find().exec();
+        } catch (error) {
+            console.log(error);
+        }
 
         res.render("index", {categories: categories, users: users, currentUser: req.user});
     },
 
     updateOne: async function (req, res) {
-        await Category.findByIdAndUpdate(req.params.id, 
-        {
-            name: req.body.name,
-            taskList: req.body.taskList
-        });
+        try {
+            await Category.findByIdAndUpdate(req.params.id, 
+            {
+                name: req.body.name,
+                taskList: req.body.taskList
+            });
+        } catch (error) {
+            console.log(error);
+        }
+
         res.redirect('/dashboard');
     },
 
     deleteOne: async function (req, res) {
-        await Category.findByIdAndDelete(req.params.id);
-        res.redirect('/dashboard');        
+        try {
+            await Category.findByIdAndDelete(req.params.id);
+        } catch (error) {
+            console.log(error);
+        }
+      
+        res.redirect('/dashboard'); 
     }
 }
--- routers/task.js
+++ routers/task.js
@@ -3,42 +3,64 @@ const Category = require("../models/category");
 
 module.exports = {
     createOne: async function (req, res) {
-        let task = new Task(req.body);
-        await task.save();
-
-        await Category.findByIdAndUpdate(req.params.id, { $push: {taskList: task._id} })
+        try {
+            let task = new Task(req.body);
+            await task.save();
+    
+            await Category.findByIdAndUpdate(req.params.id, { $push: {taskList: task._id} })
+        } catch (error) {
+            console.log(error);
+        }
 
         res.redirect('/dashboard');
     },
 
     getAll: async function (req, res) {
-        let tasks = await Task.find()
-        .populate('userList')
-        .exec();
+        let tasks = []
+
+        try {
+            tasks = await Task.find()
+                .populate('userList')
+                .exec();
+            
+        } catch (error) {
+            console.log(error);
+        }
+
         return tasks;
     },
 
     updateOne: async function (req, res) {
-        await Task.findByIdAndUpdate(
-            req.params.id, 
-            {
-                title: req.body.name,
-                description: req.body.description,
-                dueDate: req.body.dueDate,
-                priority: parseInt(req.body.priority),
-                progress: req.body.progress,
-                userList: req.body.userList
-            }
-        );
+        try {
+            await Task.findByIdAndUpdate(
+                req.params.id, 
+                {
+                    title: req.body.name,
+                    description: req.body.description,
+                    dueDate: req.body.dueDate,
+                    priority: parseInt(req.body.priority),
+                    progress: req.body.progress,
+                    userList: req.body.userList
+                }
+            );
+        } catch (error) {
+            console.log(error);
+        }
+
         res.redirect('/dashboard');
     },
 
     deleteOne: async function (req, res) {
-        await Task.findByIdAndDelete(req.params.id);
-        await Category.updateMany(
-            { taskList: { $in: [req.params.id] } }, 
-            { $pull: { taskList: req.params.id } }
-        );
+        try {
+            await Task.findByIdAndDelete(req.params.id);
+            await Category.updateMany(
+                { taskList: { $in: [req.params.id] } }, 
+                { $pull: { taskList: req.params.id } }
+            );
+        } catch (error) {
+            console.log(error);
+        }
+        
         res.redirect('/dashboard');
     }
 }
