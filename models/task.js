00d782a7ca21908d258b7316d764f623abcdc2bf
Michael  Xavier Lie <mlie0008@student.monash.edu>
2023-10-17 23:08:13 +1100

    Added title character limit

--- models/task.js
+++ models/task.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const taskSchema = new mongoose.Schema({
     title: {
         type: String,
-        required: true
+        required: true,
+        maxLength: 20
     },
     description: String,
     dueDate: {
