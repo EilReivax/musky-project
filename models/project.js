c55f8c7916daef5e92605c944f3a52c298cf29a7
Michael  Xavier Lie <mlie0008@student.monash.edu>
2023-10-18 15:05:23 +1100

    Added project model

--- models/project.js
+++ models/project.js
@@ -3,19 +3,7 @@ const mongoose = require('mongoose');
 const projectSchema = new mongoose.Schema({
     _id: String,
     startDate: Date,
-    endDate: Date,
-    categoryList: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Category'
-    }],
-    taskList: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Task'
-    }],
-    userList: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    }]
+    endDate: Date
 });
 
 module.exports = mongoose.model('Project', projectSchema);
--- routers/project.js
+++ routers/project.js
@@ -1,28 +1,36 @@
 const Project = require('../models/project');
+const Category = require('../models/category');
+const Task = require('../models/task');
+const User = require('../models/user');
 
 const PROJECT_ID = 'PROJECT_ID';
 
 module.exports = {
     getAll: async function (req, res) {
         let project = {};
+        let categories = [];
+        let tasks = [];
+        let users = [];
+
         try {
-            if (!await Project.findById(PROJECT_ID)) {
+            project = await Project.findById(PROJECT_ID);
+
+            if (!project) {
                 let newProject = new Project({ _id: PROJECT_ID });
                 await newProject.save();
             }
-            project = await Project.findById({ PROJECT_ID })
-            .populate({
-                path: 'categoryList',
-                model: 'Category',
-                populate: {
-                    path: 'taskList',
-                    model: 'Task',
-                    populate: {
-                        path: 'userList',
-                        model: 'User'
-                    }
-                }
-            })
+
+            if (!await Category.findOne()) {
+                let toDo = new Category({ title: 'To-do' });
+                let doing = new Category({ title: 'Doing' });
+                let done = new Category({ title: 'Done' });
+
+                await toDo.save();
+                await doing.save();
+                await done.save();
+            }
+
+            categories = await Category.find()
             .populate({
                 path: 'taskList',
                 model: 'Task',
@@ -31,12 +39,18 @@ module.exports = {
                     model: 'User'
                 }
             })
+            .exec();
+
+            tasks = await Task.find()
             .populate('userList')
-            .execPopulate();
+            .exec();
+
+            users = await User.find()
+            .exec();
         } catch (error) {
             res.status(400).json({ error: error });
         }
 
-        res.render("index", {project: project, currentUser: req.user});
+        res.render("index", {project: project, categories: categories, tasks: tasks, users: users, currentUser: req.user});
     },
 }
